46e85546c1224415fe4aef5408a067464b321ce6
pisekgasper <gasperpisek11@gmail.com>
2022-01-04 10:17:42 +0100

    Added Google Map functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:parkirisca/model/parking_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:permission_handler/permission_handler.dart';
+
+/* WOOZY HACKS
 Future<List<Parking>> fetchPost() async {
   final response = await http.get(
       Uri.parse('https://api.ontime.si/api/v1/parking/?format=json&page=1'));
@@ -15,6 +23,15 @@ Future<List<Parking>> fetchPost() async {
     return parsed.map<Parking>((json) => Parking.fromJson(json)).toList();
   } else {
     throw Exception('Failed to load');
+  }
+}
+*/
+
+void requestLocationPermission() async {
+  final status = await Permission.locationWhenInUse.request();
+  if (status == PermissionStatus.denied) {
+  } else if (status == PermissionStatus.permanentlyDenied) {
+    await openAppSettings();
   }
 }
 
@@ -30,24 +47,79 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late Future<List<Parking>> futureParking;
 
+  late GoogleMapController mapController;
+
+  // LJ center coordinates -> random location in LJ (46.054939, 14.504820)
+  final LatLng _center = const LatLng(46.056946, 14.505751);
+  final double _defaultZoom = 14;
+  late String _mapStyle;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    mapController.setMapStyle(_mapStyle);
+  }
+
+  final LocationSettings _locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+
   @override
   void initState() {
     super.initState();
-    futureParking = fetchPost();
+
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: _locationSettings)
+            .listen((Position position) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: _defaultZoom)));
+    });
+
+    WidgetsBinding.instance
+        ?.addPostFrameCallback((_) => requestLocationPermission());
+    //futureParking = fetchPost();
+  }
+
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Parkirisca',
       theme: ThemeData(
         primaryColor: Colors.lightBlueAccent,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Parkirisca'),
+        body: GoogleMap(
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          zoomControlsEnabled: false,
+          tiltGesturesEnabled: false,
+          rotateGesturesEnabled: false,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: _defaultZoom,
+          ),
         ),
-        body: FutureBuilder<List<Parking>>(
+      ),
+    );
+  }
+}
+
+/* WOOZY HACKS
+
+FutureBuilder<List<Parking>>(
           future: futureParking,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
@@ -66,13 +138,13 @@ class _MyAppState extends State<MyApp> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "${snapshot.data![index].name}",
-                          style: TextStyle(
+                          snapshot.data![index].name,
+                          style: const TextStyle(
                             fontSize: 18.0,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(height: 10),
+                        const SizedBox(height: 10),
                       ],
                     ),
                   ),
@@ -83,7 +155,4 @@ class _MyAppState extends State<MyApp> {
             }
           },
         ),
-      ),
-    );
-  }
-}
+ */
--- lib/model/parking_model.dart
+++ lib/model/parking_model.dart
@@ -1,22 +1,17 @@
-import 'dart:convert';
-
-List<Parking> ParkingFromJson(String str) =>
-    List<Parking>.from(json.decode(str).map((x) => Parking.fromJson(x)));
-
 class Parking {
   Parking(
-      {required this.parking_id,
+      {required this.parkingId,
       required this.name,
       required this.lat,
       required this.lng});
 
-  int parking_id;
+  int parkingId;
   String name;
   double lat;
   double lng;
 
   factory Parking.fromJson(Map<String, dynamic> json) => Parking(
-        parking_id: json["parking_id"],
+        parkingId: json["parking_id"],
         name: json["name"],
         lat: json["lat"],
         lng: json["lng"],
